eb05c7a676d8a29e2a7dac09da9347c503e654ba
magyarigr <magyarigr@gmail.com>
2013-02-28 06:59:16 +0000

    change light connections adding siblings, update prueba2.txt to set siblings for each light. Now using main loop to run simulations

--- simulation/traffic.erl
+++ simulation/traffic.erl
@@ -1,6 +1,6 @@
 -module(traffic).
 
--export([start/0, stop/0, light/4, lane/3, set_map/0, connect/1, connect_lanes/1, test/0]).
+-export([start/1, stop/0, lane/3, set_map/0, connect/1, connect_lanes/1, test/0]).
 
 -export([init/0]).
 
@@ -10,7 +10,7 @@
 
 
 %client calls
--import(light_fsm,[start_link/0,move_avenue/1, move_street/1, idle/1]).
+-import(light_fsm,[start_link/0,move_avenue/1, move_street/1, idle/1,update_siblings/2, evaluate_state/1]).
 
 
 %% This module is used to run the simulation
@@ -27,8 +27,15 @@ test_move_ca(LightId) ->
 test_idle(LightId) ->
     light_fsm:idle(LightId).
 
-start() ->
-    register(traffic, spawn(traffic, init, [])).
+start(Lapse) ->
+    register(traffic, spawn(traffic, init, [])),
+    run_simulation(Lapse, 0).
+
+run_simulation(Lapse, Current) when Current < Lapse ->
+    call(continue),   
+    run_simulation(Lapse, Current + 1);
+run_simulation(Lapse, Current) when Current >= Lapse ->
+    stop().
  
 stop() -> 
     call(stop).
@@ -38,7 +45,8 @@ call(Message) ->
     receive
        {reply, Reply} -> Reply
     end.
-  
+
+ 
 init() ->
     Ligths = set_map(), 
     loop(Ligths, 0).
@@ -51,16 +59,17 @@ set_map() ->
     {_OrigLanes, AllocatedLanes} = allocate_lanes({Roads,[]}),
   
     connect_lanes({AllocatedLanes,AllocatedLanes}),
-    LightsFSM = connect({AllocatedLights, AllocatedLanes, []}),    
+    LightsFSM = connect({AllocatedLights, AllocatedLanes, []}), 
+    set_connection_to_light_siblings(LightsFSM,LightsFSM),
     LightsFSM.
     
     
 %% Spawn every intersection and lane on the map
 allocate_lights({[],Spawned})->
     {[],Spawned};
-allocate_lights({[{LightId,_ManagedLanes, State, On_time, Go_time}|Tail], Spawned}) ->
-    %%Pid = spawn(traffic,light, [ManagedLanes,State, On_time, Go_time]),
-    allocate_lights({Tail, [{LightId,State, On_time, Go_time} | Spawned]}). 
+allocate_lights({[{LightId,_ManagedLanes, Siblings, Cycle_time, Go_time}|Tail], Spawned}) ->
+    %%Pid = spawn(traffic,light, [ManagedLanes,Siblings, Cycle_time, Go_time]),
+    allocate_lights({Tail, [{LightId,Siblings, Cycle_time, Go_time} | Spawned]}). 
           
 allocate_lanes({[], Spawned}) ->
     {[],Spawned};
@@ -71,11 +80,11 @@ allocate_lanes({[{LaneId,LightController,Dir, Type, ConnectedLanes, CarsQueque}|
 %%connect lights with its lanes
 connect({[], _LaneList, LightsFSM}) -> 
     LightsFSM;
-connect({[{LightId,State, On_time, Go_time} | TailLight], LaneList,LightsFSM}) -> 
+connect({[{LightId,Siblings, Cycle_time, Go_time} | TailLight], LaneList,LightsFSM}) -> 
     io:format("Luz: ~w / lineas: ~w.~n",[LightId, LaneList]),
     {ManagedLanes, RemLanes} = find_lanes({LightId, {[],[]}}, LaneList, []),
     io:format("Managed Lanes: ~w.~n~n",[ManagedLanes]),
-    {ok, LightPid} = light_fsm:start_link({ManagedLanes,State, On_time, Go_time}),
+    {ok, LightPid} = light_fsm:start_link({ManagedLanes,Siblings, Cycle_time, Go_time}),
     connect({TailLight, RemLanes, [{LightId,LightPid}|LightsFSM]}).
 
 find_lanes({LightId,ManagedLanes}, [], RemLanes) -> 
@@ -140,6 +149,25 @@ find_adjLanes({LaneId,LanePid}, AdjLaneId, [{AdjLaneId,AdjLanePid, _Light,_Dir, 
 find_adjLanes({LaneId, LanePid}, AdjLaneId, [_LHead | LTail]) ->
     io:format("No Coincidencia: ~w continue.~n",[{LaneId, LanePid}]),
     find_adjLanes({LaneId, LanePid}, AdjLaneId, LTail).
+
+%% Connect traffic lights with siblings
+set_connection_to_light_siblings([], _LightsList) -> [];
+set_connection_to_light_siblings([{Id, Pid}|LightsRem], LightsList) ->
+    {State, Cycle_time, Go_time, Siblings} = light_fsm:get_state(Pid),
+    io:format("Buscando hermanos de: ~w en ~w.~n",[{Id,Pid},Siblings]),
+    CompleteSiblings = find_siblings(Siblings,LightsList,[]),
+    io:format("Hermanos encontrados ~w.~n",[CompleteSiblings]),
+    
+    light_fsm:update_siblings(Pid, CompleteSiblings),
+    set_connection_to_light_siblings(LightsRem,LightsList).
+    
+find_siblings([],_LightsList, CompleteSiblings) ->
+    lists:reverse(CompleteSiblings);
+find_siblings([SiblingId|Tail], LightsList,CompleteSiblings) ->
+    SiblingData = lists:keyfind(SiblingId, 1, LightsList), 
+    find_siblings(Tail,LightsList, [SiblingData|CompleteSiblings]).
+    
+
         
 %%text file initialization of map
 get_lights() -> 
@@ -186,107 +214,96 @@ loop(Intersections, Time) ->
   %% according to the state of the FSM it will or will not move  the cars
   
   receive
-      {call, Pid, continue} ->    	  
-          UpdateIntersections = 
-              lists:foreach
-              (
-                  fun(Intersection)->          				      
-	  	      {State, On_time, Go_time} = light_fsm:get_state(Intersection),
-	  	      if
-	  	          On_time > Go_time ->
-	  	              light_fsm:idle(Intersection),
-			      case State of
-			          greenred -> light_fsm:move_street(Intersection);
-				  redgreen -> light_fsm:move_avenue(Intersection)
-			      end;
-			  On_time < Go_time ->
-			      case State of
-			          greenred -> light_fsm:move_avenue(Intersection);
-				  redgreen -> light_fsm:move_street(Intersection)
-			      end;
-	  	          true ->
-	  	              light_fsm:idle(Intersection)
-	  	      end
-	   	  end,
-	   	  Intersections
-	      ),
-	  reply(Pid, {iteration, ok}),
-	  loop(UpdateIntersections, Time + 1);
-    {call,Pid, stop} -> {finish, Time}
+      {call, Pid, continue} ->
+         io:format("Running simulation iteration: ~w continue. Intersections ~w~n",[Time, Intersections]),
+         lists:foreach
+             (
+                 fun({InterId, Intersection})->
+                     io:format("Evaluating state for light_fsm: ~w continue.~n",[InterId]),
+                     light_fsm:evaluate_state(Intersection)
+                     %%Send to light_fsm          				      
+	             %%{State, Cycle_time, Go_time} = light_fsm:get_state(Intersection),
+	 	     %%Next_time = Go_time + 1,
+	 	     %%if
+	  	     %%    Next_time >= Cycle_time ->
+	  	     %%        light_fsm:idle(Intersection),
+		     %%        case State of
+		     %%            greenred -> light_fsm:move_street(Intersection);
+		     %%            redgreen -> light_fsm:move_avenue(Intersection)
+		     %%        end;
+		     %%    Next_time < Cycle_time ->
+		     %%        case State of
+		     %%          redred   -> light_fsm:estimate_after_idle(Intersection);
+		     %%          greenred -> light_fsm:move_avenue(Intersection);
+		     %%	  redgreen -> light_fsm:move_street(Intersection)
+		     %%        end;
+	  	     %%    true ->
+	  	     %%        light_fsm:idle(Intersection)
+	  	     %%end
+	  	 end,
+	  	 Intersections
+	     ),
+	 reply(Pid, {iteration, ok}),
+	 io:format("Finishing simulation iteration: ~w continue. Intersections~n",[Time]),
+	 io:format("----------------------------------------------------------~n~n"),
+	 loop(Intersections, Time + 1);
+    {call,Pid, stop} -> 
+         reply(Pid, {normal, Time})
   end.
 
 reply (Pid, Reply) ->
     Pid ! {reply, Reply}.
 
-changeState(Intersection,State) ->
-    light_fsm:idle(Intersection),
-    case State of
-        greenred -> light_fsm:move_street(Intersection);
-        redgreen -> light_fsm:move_avenue(Intersection)
-    end.
 
 %%% DEPRECATED AFTER USE OF FSM, FUNCTIONS MOVED TO LIGHT_FSM.ERL
-light(ManagedLanes, State, Go_time, On_time) ->
-    receive
-        {connection, Pid, active} -> 
-    		NewState = update_onActive(ManagedLanes, Go_time, On_time, self(), State),
-    		light(ManagedLanes, NewState, Go_time, On_time + 1);
-        {connection, Pid, idle}   ->
-		update_onIdle(ManagedLanes, Go_time, On_time, self(), State),
-    		light(ManagedLanes, idle, Go_time, On_time + 1);
-        {connect_lane, Lane_Pid, Pid, Dir}  -> 
-    		{Av, Ca} = ManagedLanes,
-    		io:format("{Av ~w, Ca: ~w.~n",[Av, Ca]),
-    		if
-    		  Dir =:= av -> 
-    		  	NewManaged = {[Lane_Pid| Av], Ca},
-    		  	reply(Pid,ok),
-    		  	io:format("light ~w, lanes: ~w.Dir:~w~n",[self(), NewManaged, Dir]),
-    		  	light(NewManaged, State, Go_time, On_time);
-    		  Dir =:= ca ->
-    			NewManaged = {Av, [Lane_Pid| Ca]},
-    			reply(Pid,ok),
-    			io:format("light ~w, lanes: ~w. Dir:~w~n ",[self(), NewManaged,Dir]),
-    		  	light(NewManaged, State, Go_time, On_time);
-     	  	  true ->
-     	  	  	io:format("NO TYPE MATCH",[]),
-     	  	  	reply(Pid,ok), 
-     	  	  	light(ManagedLanes, State, Go_time, On_time)     	  	  	
-     	  	end;
-        stop ->	      
-    		{ok, normal}
-    end.
+%%light(ManagedLanes, State, Go_time, Cycle_time) ->
+%%    receive
+%%        {connection, Pid, active} -> 
+%%    		NewState = update_on_active(ManagedLanes, Go_time, Cycle_time, self(), State),
+%%    		light(ManagedLanes, NewState, Go_time, Cycle_time + 1);
+%%        {connection, Pid, idle}   ->
+%%		update_on_idle(ManagedLanes, Go_time, Cycle_time, self(), State),
+%%    		light(ManagedLanes, idle, Go_time, Cycle_time + 1);
+%%        {connect_lane, Lane_Pid, Pid, Dir}  -> 
+%%    		{Av, Ca} = ManagedLanes,
+%%    		io:format("{Av ~w, Ca: ~w.~n",[Av, Ca]),
+%%    		if
+%%    		  Dir =:= av -> 
+%%    		  	NewManaged = {[Lane_Pid| Av], Ca},
+%%    		  	reply(Pid,ok),
+%%    		  	io:format("light ~w, lanes: ~w.Dir:~w~n",[self(), NewManaged, Dir]),
+%%    		  	light(NewManaged, State, Go_time, Cycle_time);
+%%    		  Dir =:= ca ->
+%%    			NewManaged = {Av, [Lane_Pid| Ca]},
+%%    			reply(Pid,ok),
+%%    			io:format("light ~w, lanes: ~w. Dir:~w~n ",[self(), NewManaged,Dir]),
+%%    		  	light(NewManaged, State, Go_time, Cycle_time);
+%%     	  	  true ->
+%%     	  	  	io:format("NO TYPE MATCH",[]),
+%%     	  	  	reply(Pid,ok), 
+%%     	  	  	light(ManagedLanes, State, Go_time, Cycle_time)     	  	  	
+%%     	  	end;
+%%        stop ->	      
+%%    		{ok, normal}
+%%    end.
   
-update_onActive(Lanes, Go_time, On_time, Pid, State) ->  
-%% ver como actualizar los valores ver si requiere ser recursivo o proceso
-    lists:foreach(fun(LaneId)->
-		   %%{LaneId,Type, _Cars} = Lane,	 		 		    
-			LaneId ! {go, Pid, On_time}
-			end,
-		Lanes
- 	       ).
-update_onIdle(Lanes, Go_time, On_time, Pid, State) ->  
-%% actualizar todos los carros en espera
-    lists:foreach(fun(LaneId)->
-		   %%{LaneId,Type, _Cars} = Lane,	 		 		    
-			LaneId ! {waiting, Pid, On_time}
-			end,
-		Lanes
- 	       ).
-  
-  
+ 
 lane(Type, ConnectedLanes, CarsQueque) ->
   %% For each lane on every street in the system, 
   %% get the move acording to the ligth state passed 
   %% and run the complete simulation
     receive
-        {go, LightController, Time} -> %% if a go message is received it tells all cars to start to move
+        {go, LightController,TimeCycle, Time} -> %% if a go message is received it tells all cars to start to move
 		NewCarsQueque = move_cars(CarsQueque, LightController),
+		io:format("Moving msj received from ~w.~n",[LightController]),
+		reply(LightController, updated),
 		lane(Type, ConnectedLanes, NewCarsQueque);
            
 	{stop, LightController} ->  
 	%% send a message to all cars to start to stop preventing them to pass the red ligth
 		NewCarsQueque = stop_cars(CarsQueque,LightController),
+		io:format("Stop msj received from ~w.~n",[LightController]),
+		reply(LightController, updatedStop),
 		lane(Type, ConnectedLanes, NewCarsQueque);
       
 	{dispatch, LightController, Time} ->		
@@ -296,6 +313,8 @@ lane(Type, ConnectedLanes, CarsQueque) ->
 		lane(Type, ConnectedLanes, NewCarsQueque);
 	{waiting, LightController, Time} ->
 		NewCarsQueque = waiting(CarsQueque,Time),
+		io:format("Waiting msj received from ~w.~n",[LightController]),
+		reply(LightController, updatedWaiting),
 		lane(Type, ConnectedLanes, NewCarsQueque);
 	
 	{connect_lane, AdjLane_Pid, Pid}  ->     		
@@ -306,14 +325,17 @@ lane(Type, ConnectedLanes, CarsQueque) ->
     		lane(Type, NewConnectedLanes, CarsQueque);  	  	
 	{test, Pid, Msg} ->
 	        io:format("Message received from light ~w. ~w~n",[Pid, Msg]),
-	        reply(Pid, copiado);	
+	        reply(Pid, copiado),
+	        lane(Type, ConnectedLanes, CarsQueque);	
 	stop -> {ok, normal}
-	%%{connect_light, Light_Pid} ->
-	%%	lanes(Light_Pid, Type, ConnectedLanes, CarsQueque)	
     end.  
 
-move_cars([],_) -> [];
-move_cars([FirstCar|Waiting], Pid) -> true.
+move_cars([],_) -> 
+    io:format("No more cars to move ~n",[]),
+    [];
+move_cars([FirstCar|Waiting], Pid) -> 
+    io:format("moving cars ~n",[]),
+    true.
     %%{go, Line1, Line2, Time} -> true.
 
 stop_cars([], _) -> [];
@@ -321,7 +343,6 @@ stop_cars([FirstCar|Waiting], Pid) -> true.
 
 dispatch_cars([], _ConnectedLanes, _Time, Pid) -> [];
 dispatch_cars([FirstCar|Waiting], ConnectedLanes, Time, Pid) -> true.
-
 
 waiting([],_) -> [];
 waiting([FirstCar|Waiting], Pid) -> true.  
