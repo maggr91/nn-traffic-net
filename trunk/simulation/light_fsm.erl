fa300889159551ce57bc191d443e79b656c8a7f5
magyarigr@gmail.com <magyarigr@gmail.com@3129182c-86b3-417b-8e0b-2994bc4433b6>
2013-03-07 04:11:48 +0000

    change light_fsm.erl now using sync event calls

--- trunk/simulation/light_fsm.erl
+++ trunk/simulation/light_fsm.erl
@@ -18,16 +18,19 @@
 %%client functions
 
 move_avenue(Pid) ->
-    gen_fsm:send_event(Pid,move_avenue).
+    gen_fsm:sync_send_event(Pid,move_avenue).
     
 move_street(Pid) ->
-    gen_fsm:send_event(Pid,move_street).
+    gen_fsm:sync_send_event(Pid,move_street).
 
 idle(Pid) ->
-    gen_fsm:send_event(Pid,idle).
+    gen_fsm:sync_send_event(Pid,idle).
 
 update_siblings(Pid, Siblings) ->
     gen_fsm:sync_send_event(Pid,{update_siblings, Siblings}).
+    
+update_csu(Pid, CSU) ->
+    gen_fsm:sync_send_event(Pid,{update_csu, CSU}).
         
 %% gen_fsm functions
 
@@ -37,7 +40,9 @@ start_link(Args) ->
 init(Args) ->
     io:fwrite("gen_fsm called ~w:init(~w)~n", [?MODULE, Args]),
     %{ok, redred,#state{}}.
-    {ok, redred,Args}.
+    {LightId, {Av, Ca},Siblings, Cycle_time, Go_time, LogData} = Args,
+    
+    {ok, redred,{LightId, {Av, Ca},Siblings, Cycle_time, Go_time, true, LogData}}.
 
 get_state(LightPid) ->
     try
@@ -78,57 +83,113 @@ code_change(OldVsn, StateName, StateData, Extra) ->
 %% IDLE STATE
 redred(move_avenue, StateData) ->
     io:format("Moving avenue lanes. Data: ~w~n",[StateData]),
-    {{Av, _Ca},_Siblings, Cycle_time, _Go_time} = StateData,
-    update_on_idle(Av, Cycle_time),
-    {next_state, greenred, StateData};
+    {LightId, {Av, Ca},Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    {next_state, greenred, {LightId, {Av, Ca},Siblings, Cycle_time, Go_time, ReadyAv and ReadyCa, LogData}};
 redred(move_street, StateData) ->
     io:format("Moving street lanes. Data: ~w~n",[StateData]),
-    {{_Av, Ca},_Siblings, Cycle_time, _Go_time} = StateData,
-    update_on_idle(Ca, Cycle_time),
-    {next_state, redgreen, StateData};
+    {LightId, {Av, Ca}, Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    {next_state, redgreen, {LightId, {Av, Ca}, Siblings, Cycle_time, Go_time, ReadyCa and ReadyAv, LogData}};
 redred(Event, StateData) ->
     unexpected_event(redred, Event, StateData),
     {next_state, redred, StateData}.
-redred(get_state, _From, StateData = {_ManagedLanes,Siblings, Cycle_time, Go_time}) ->
-    {reply, {redred,Cycle_time, Go_time,Siblings},redred, StateData};
+%%Synch calls
+redred(get_state, _From, StateData = {_LightId, _ManagedLanes,Siblings, Cycle_time, Go_time, CSU, LogData}) ->
+    {reply, {redred,Cycle_time, Go_time,Siblings, CSU, LogData},redred, StateData};
 redred({update_siblings, Siblings},_From, StateData) ->
     io:format("Updating StateData: ~w~n",[Siblings]),
-    {{Av, Ca},_Siblings, Cycle_time, Go_time} = StateData,
-    {reply, {redred,Siblings},redred, {{Av, Ca}, Siblings, Cycle_time, Go_time}}.
+    {LightId,{Av, Ca},_Siblings, Cycle_time, Go_time, CSU, LogData} = StateData,
+    {reply, {redred,Siblings},redred, {LightId,{Av, Ca}, Siblings, Cycle_time, Go_time, CSU, LogData}};
+redred({update_csu, NewCSU},_From, StateData) ->
+    io:format("Updating CSU: ~w~n",[NewCSU]),
+    {LightId,{Av, Ca},Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    {reply, {redred,NewCSU},redred, {LightId,{Av, Ca}, Siblings, Cycle_time, Go_time, NewCSU, LogData}};
+redred(move_avenue,_From, StateData) ->
+    io:format("Moving avenue lanes. Data: ~w~n",[StateData]),
+    {LightId,{Av, Ca},Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    {reply, {redred,Siblings},greenred, {LightId,{Av, Ca},Siblings, Cycle_time, Go_time, ReadyAv and ReadyCa, LogData}};
+redred(move_street,_From, StateData) ->
+    io:format("Moving street lanes. Data: ~w~n",[StateData]),
+    {LightId,{Av, Ca}, Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    {reply, {redred,Siblings},redgreen, {LightId,{Av, Ca}, Siblings, Cycle_time, Go_time, ReadyCa and ReadyAv, LogData}}.
 
 %% Moving AVENUES       
 greenred(move_avenue, StateData) ->
     io:format("continue moving avenue lanes. Data: ~w~n",[StateData]),
-    {{Av, Ac},Siblings, Cycle_time, Go_time} = StateData,
-    update_on_active(Av, Cycle_time, Go_time), 
-    {next_state, greenred, {{Av, Ac},Siblings, Cycle_time, Go_time + 1}};
+    {LightId,{Av, Ca},Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    ReadyAv = update_on_active(Av, Cycle_time, Go_time), 
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    {next_state, greenred, {LightId,{Av, Ca},Siblings, Cycle_time, Go_time + 1, ReadyAv and ReadyCa, LogData}};
 greenred(idle, StateData) ->
     io:format("stop moving avenue lanes. Data: ~w~n",[StateData]),
-    {{Av, Ca},Siblings, Cycle_time, _Go_time} = StateData,
-    update_on_idle(Av, Cycle_time),
-    {next_state, redred, {{Av, Ca},Siblings, Cycle_time, 0}};
+    {LightId,{Av, Ca},Siblings, Cycle_time, _Go_time, _CSU, LogData} = StateData,
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    {next_state, redred, {LightId,{Av, Ca},Siblings, Cycle_time, 0, ReadyAv and ReadyCa, LogData}};
 greenred(Event, StateData) ->
     unexpected_event(greenred, Event, StateData),
     {next_state, greenred, StateData}.
-greenred(get_state, _From, StateData = {_ManagedLanes,Siblings, Cycle_time, Go_time}) ->
-    {reply, {greenred,Cycle_time, Go_time, Siblings},greenred, StateData}.
+%%Synch calls
+greenred(get_state, _From, StateData = {_LightId,_ManagedLanes,Siblings, Cycle_time, Go_time, CSU, LogData}) ->
+    {reply, {greenred,Cycle_time, Go_time, Siblings, CSU,LogData},greenred, StateData};
+greenred({update_csu, NewCSU},_From, StateData) ->
+    io:format("Updating CSU: ~w~n",[NewCSU]),
+    {LightId,{Av, Ca},Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    {reply, {greenred,NewCSU},greenred, {LightId,{Av, Ca}, Siblings, Cycle_time, Go_time, NewCSU, LogData}};
+greenred(move_avenue, _From, StateData) ->
+    io:format("continue moving avenue lanes. Data: ~w~n",[StateData]),
+    {LightId,{Av, Ca},Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    ReadyAv = update_on_active(Av, Cycle_time, Go_time), 
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    {reply, {greenred,Siblings},greenred, {LightId,{Av, Ca},Siblings, Cycle_time, Go_time + 1, ReadyAv and ReadyCa, LogData}};
+greenred(idle, _From, StateData) ->
+    io:format("stop moving avenue lanes. Data: ~w~n",[StateData]),
+    {LightId,{Av, Ca},Siblings, Cycle_time, _Go_time, _CSU, LogData} = StateData,
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    {reply, {greenred,Siblings},redred, {LightId,{Av, Ca},Siblings, Cycle_time, 0, ReadyAv and ReadyCa, LogData}}.
 
 %% Moving STREETS    
 redgreen(move_street, StateData) ->
     io:format("continue moving street lanes. Data: ~w~n",[StateData]),
-    {{Av, Ca},Siblings, Cycle_time, Go_time} = StateData,
-    update_on_active(Ca, Cycle_time, Go_time),
-    {next_state, redgreen, {{Av, Ca},Siblings, Cycle_time, Go_time + 1}};
+    {LightId,{Av, Ca},Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    ReadyCa = update_on_active(Ca, Cycle_time, Go_time),
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    {next_state, redgreen, {LightId,{Av, Ca},Siblings, Cycle_time, Go_time + 1, ReadyCa and ReadyAv, LogData}};
 redgreen(idle, StateData) ->
     io:format("stop moving street lanes. Data: ~w~n",[StateData]),
-    {{Av, Ca},Siblings, Cycle_time, _Go_time} = StateData,
-    update_on_idle(Ca, Cycle_time),
-    {next_state, redred, {{Av, Ca},Siblings, Cycle_time, 0}};
+    {LightId,{Av, Ca},Siblings, Cycle_time, _Go_time, _CSU, LogData} = StateData,
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    {next_state, redred, {LightId,{Av, Ca},Siblings, Cycle_time, 0, ReadyCa and ReadyAv, LogData}};
 redgreen(Event, StateData) ->
     unexpected_event(redgreen, Event, StateData),
     {next_state, redgreen, StateData}.      
-redgreen(get_state, _From, StateData = {_ManagedLanes,Siblings, Cycle_time, Go_time}) ->
-    {reply, {redgreen,Cycle_time, Go_time,Siblings},redgreen, StateData}.
+redgreen(get_state, _From, StateData = {_LightId,_ManagedLanes,Siblings, Cycle_time, Go_time, CSU, LogData}) ->
+    {reply, {redgreen,Cycle_time, Go_time,Siblings, CSU, LogData},redgreen, StateData};
+redgreen({update_csu, NewCSU},_From, StateData) ->
+    io:format("Updating CSU: ~w~n",[NewCSU]),
+    {LightId,{Av, Ca},Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    {reply, {redgreen,NewCSU},redgreen, {LightId,{Av, Ca}, Siblings, Cycle_time, Go_time, NewCSU, LogData}};
+redgreen(move_street,_From, StateData) ->
+    io:format("continue moving street lanes. Data: ~w~n",[StateData]),
+    {LightId,{Av, Ca},Siblings, Cycle_time, Go_time, _CSU, LogData} = StateData,
+    ReadyCa = update_on_active(Ca, Cycle_time, Go_time),
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    {reply, {redgreen,Siblings},redgreen, {LightId,{Av, Ca},Siblings, Cycle_time, Go_time + 1, ReadyCa and ReadyAv, LogData}};
+redgreen(idle,_From, StateData) ->
+    io:format("stop moving street lanes. Data: ~w~n",[StateData]),
+    {LightId,{Av, Ca},Siblings, Cycle_time, _Go_time, _CSU, LogData} = StateData,
+    ReadyCa = update_on_idle(Ca, Cycle_time),
+    ReadyAv = update_on_idle(Av, Cycle_time),
+    {reply, {redgreen,Siblings},redred, {LightId,{Av, Ca},Siblings, Cycle_time, 0, ReadyCa and ReadyAv, LogData}}.
     
 %% private functions
 unexpected_event(_CurrentState, _Event, _StateData) ->
@@ -139,47 +200,58 @@ test() ->
  
 evaluate_state(LightId) ->
    io:format("Evaluating state ~w~n",[LightId]),
-   {State, Cycle_time, Go_time, _Siblings} = get_state(LightId),
-   Next_time = Go_time + 1,
-   if
-     Next_time >= Cycle_time ->
-         io:format("Finishing ~w way cycle moving to idle~n",[State]),
-         idle(LightId),
-         io:format("Changing to ~w way cycle from idle~n",[State]),
-	 case State of             
-	     greenred -> move_street(LightId);
-	     redgreen -> move_avenue(LightId)
-	 end;
-     Next_time < Cycle_time ->
-         io:format("Continuing ~w way cycle~n",[State]),
-	 case State of
-	     redred   -> estimate_after_idle(LightId);
-	      		 %%evaluate_state(LightId);           
-	     greenred -> move_avenue(LightId);
-	     redgreen -> move_street(LightId)
-	 end;
-   true ->
-	 idle(LightId)
+   {State, Cycle_time, Go_time, _Siblings, CSU, LogData} = get_state(LightId),
+   io:format("CSU: ~w~n",[CSU]),
+   
+   case CSU of
+       true ->
+           Next_time = Go_time + 1,
+	   update_csu(LightId,false),
+	   if
+	     Next_time >= Cycle_time ->
+		 io:format("Finishing ~w way cycle moving to idle~n",[State]),
+		 idle(LightId),
+		 io:format("Changing to ~w way cycle from idle~n",[State]),
+		 case State of             
+		     greenred -> move_street(LightId);
+		     redgreen -> move_avenue(LightId)
+		 end;
+	     Next_time < Cycle_time ->
+		 io:format("Continuing ~w way cycle~n",[State]),
+		 case State of
+		     redred   -> estimate_after_idle(LightId);
+		      		 %%evaluate_state(LightId);           
+		     greenred -> move_avenue(LightId);
+		     redgreen -> move_street(LightId)
+		 end;
+	   true ->
+		 idle(LightId)
+	   end,
+	   Waiting = false;
+       false ->
+           Waiting = LightId
    end,
-   io:format("-----------------------------------------------~n~n").
+   io:format("Waiting: ~w--------------------------------------~n",[Waiting]),
+   io:format("-----------------------------------------------~n~n"),
+   Waiting.
 
 %% Update each lane that is active on this light 
 update_on_active([], _Cycle_time, _Go_time) ->  
-    [];
-update_on_active([LaneId|Tail], Go_time, Cycle_time) ->  
-    LaneId ! {go, self(), Cycle_time, Go_time},
+    true;
+update_on_active([{LaneId,LanePid}|Tail], Go_time, Cycle_time) ->  
+    LanePid ! {go, self(), Cycle_time, Go_time},
     receive
-        {reply, _Reply} -> io:format("reply recieve after update on active lane ~n")
+        {reply, _Reply} -> io:format("reply recieve after update on active lane ~w.~n",[LaneId])
     end,
     update_on_active(Tail,Cycle_time, Go_time).
 
 %% Update each lane that is waiting on this light 
 update_on_idle([], _Cycle_time) -> 
-    [];
-update_on_idle([LaneId|Tail], Cycle_time) ->
-    LaneId ! {waiting, self(), Cycle_time},
+    true;
+update_on_idle([{LaneId,LanePid}|Tail], Cycle_time) ->
+    LanePid ! {waiting, self(), Cycle_time},
     receive
-        {reply, _Reply} -> io:format("reply recieve after update on idle lane ~n")
+        {reply, _Reply} -> io:format("reply recieve after update on idle lane ~w.~n",[LaneId])
     end,
     update_on_idle(Tail, Cycle_time).
     
@@ -192,3 +264,7 @@ estimate_after_idle(LightPid) ->
              move_street(LightPid),
              move_street(LightPid)
     end.
+    
+%% Write down results
+write_result(Data, Path) ->
+    file:write_file(Path, io_lib:fwrite("~p.\n", [Data]),[append]).
