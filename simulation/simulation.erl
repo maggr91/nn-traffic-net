0c81526a6d67584a7ce405994bac1648ed4f68bf
magyarigr <magyarigr@gmail.com>
2013-05-15 22:45:36 +0000

    Fix stop after dispatch error

--- simulation/simulation.erl
+++ simulation/simulation.erl
@@ -4,7 +4,8 @@
 
 
 start(Lapse) ->    
-    register(poissonServer,prob:start({poisson,6})),    
+    register(poissonServer,prob:start({poisson,6})),
+    register(geoServer,prob:start({geometrica,0.5})),    
     register(traffic, traffic:start()),
     timer:apply_after(200, simulation, run_simulation, [Lapse]).
     %%run_simulation(Lapse).
