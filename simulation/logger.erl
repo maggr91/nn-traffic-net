1aed75deb4eab1860d73a2a4f9f36bbd1e35e8b5
magyarigr <magyarigr@gmail.com>
2013-12-16 03:15:18 +0000

    Add logger module to contain al modules files to log operations. Trainer_mod working with checkpoints using a good delay because of perceptron processing time and # of training sets (saving training to checkpoint/nn_saving/)

---
+++ simulation/logger.erl
@@ -0,0 +1,38 @@
+-module(logger).
+-export([start/0, debug_sm/2, debug_ann/2, debug_lane/2]).
+
+-export([init/0]).
+
+start() ->
+	filelib:ensure_dir("logs/"),
+	spawn(?MODULE, init, []).
+	
+init() ->
+	Files = [{simulation, "/logs/simulation_log.txt"}, {ann, "/logs/ann_log.txt"}, {lane, "/logs/lanes_log.txt"}],
+	{ok, Cwd} = file:get_cwd(),
+	
+	Logs = lists:map(fun({Id, File}) -> {Id, Cwd ++ File} end, Files),
+	io:format("FILES READY ~p",[Logs]),
+	listen(Logs).
+	
+listen(Logs) ->
+	receive
+		{log, Type, Data} -> %io:format("Lookgin for ~w in ~w", [Type, Logs]),
+							 {Type, File} = lists:keyfind(Type, 1, Logs),
+							 %io:format("File ~p", [File]),
+							 filemanager:write_result(File, Data),
+							 listen(Logs);
+		stop -> {normal, ?MODULE};
+		status -> io:format("Test ~n")
+	end.
+
+
+debug_sm(Logger, Data) ->
+	Logger ! {log, simulation, Data}.
+	
+debug_ann(Logger, Data) ->
+	%io:format("Log ~w ann data ",[Logger]),
+	Logger ! {log, ann, Data}.
+	
+debug_lane(Logger, Data) ->
+	Logger ! {log, lane, Data}.
