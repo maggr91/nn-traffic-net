2233e695ee09b268e968cc6584613fd835ecb52a
magyarigr@gmail.com <magyarigr@gmail.com@3129182c-86b3-417b-8e0b-2994bc4433b6>
2013-12-16 03:15:18 +0000

    Add logger module to contain al modules files to log operations. Trainer_mod working with checkpoints using a good delay because of perceptron processing time and # of training sets (saving training to checkpoint/nn_saving/)

--- trunk/simulation/perceptron.erl
+++ trunk/simulation/perceptron.erl
@@ -85,13 +85,10 @@ dot_prod([Wj | WTail] , [Xi | XTail], Sum) ->
 %% The next dot product sum, first applies the product and after that sum all
 %% VERIFICAR INEFICIENCIA YA QUE TIENE QUE RECORRER LA LISTA DOS VECES 
 %% UNA PARA MULTIPLICAR Y OTRA PARA SUMAR
-vector_map(Func, X, Y) ->
-	%%io:format("VECTOR MAP PARAM FUNCTION: ~p~n", [{Func, X,Y}]),
-	vector_map(Func, X, Y, []).
-vector_map(_Func, [], [], Map) ->
-	Map;
-vector_map(Func, [Xhead | Xtail], [Yhead | Ytail], Map) ->
-	vector_map(Func, Xtail, Ytail, [Func(Xhead, Yhead) | Map]).
+vector_map(_Func, [], []) ->
+	[];
+vector_map(Func, [Xhead | Xtail], [Yhead | Ytail]) ->
+	[Func(Xhead, Yhead) | vector_map(Func, Xtail, Ytail)].
 	
 dot_product(X, Y) ->
 	lists:foldl(fun(Element, Sum) -> Element + Sum end, 0,
@@ -121,23 +118,23 @@ perceptron(LayerId, Weights, Inputs, Sensitivities) ->
 	receive
 		{stimulate, Input, IsTraining} ->
 			%%add input to the list
-			io:format("STIMULATE CALLED ON ~w~n", [{{LayerId, self()},Inputs, Sensitivities}]),
-			io:format("Replace input ~w on ~w for ~w~n~n", [Input, Inputs, {LayerId,self()}]),
+			%io:format("STIMULATE CALLED ON ~w~n", [{{LayerId, self()},Inputs, Sensitivities}]),
+			%io:format("Replace input ~w on ~w for ~w~n~n", [Input, Inputs, {LayerId,self()}]),
 			New_Inputs = replace_input(Inputs, Input),
 			
 			%%calculate output of the perceptron
-			io:format("Calculate new output on ~w for ~w~n", [{LayerId,self()}, {Sigmoid, Weights, convert_to_values(New_Inputs)}]),
+			%io:format("Calculate new output on ~w for ~w~n", [{LayerId,self()}, {Sigmoid, Weights, convert_to_values(New_Inputs)}]),
 			Output = feed_forward(Sigmoid, Weights, convert_to_values(New_Inputs)),
-			io:format("New output ~w on ~w~n", [Output, {LayerId,self()}]),
+			%io:format("New output ~w on ~w~n", [Output, {LayerId,self()}]),
 			
 			%%stimulate next layer
 			if 	Sensitivities =/= [] ->
 					%% the output is sended to at least one perceptron
-					stimulate_next_layer(LayerId, convert_to_keys(Sensitivities), Output, IsTraining);
+					stimulate_next_layer(LayerId, convert_to_keys(Sensitivities), Output, IsTraining);					
 				Sensitivities =:= [] ->
 					%% there are no perceptron connected i.e output neuron
-					io:format("LEARNING ~w", [{LayerId,self()}]),
-					io:format("~n~w outputs: ~w", [{LayerId,self()}, Output]),
+					%io:format("LEARNING ~w", [{LayerId,self()}]),
+					%io:format("~nOUTPUT LAYER NEURON ~w output: ~w", [{LayerId,self()}, Output]),
 					%% call learn message to self()
 					%%ON TEST Get the desired output
 					%network ! {desired_output, self(), LayerId},
@@ -152,29 +149,35 @@ perceptron(LayerId, Weights, Inputs, Sensitivities) ->
 					%%% NEW FOR NN DATE 01/12/2013
 					case IsTraining of
 						{true, DesiredOutputList} -> 
-							io:format("~nIs Training to output ~w to neuron ~w~n~n", [DesiredOutputList, LayerId]),
+							%io:format("~nIs Training to output ~w to neuron ~w~n~n", [DesiredOutputList, LayerId]),
 							DesiredOutput = get_desired_output(LayerId, DesiredOutputList),
-							io:format("Desired Output retrieved ~w~n~n", [DesiredOutput]),
-							self() ! {learn, {{LayerId, self()}, DesiredOutput}};
+														
+							logger(LayerId, Output, DesiredOutput),
+							
+							%io:format("INPUTS B4 learing ~w~n~n", [New_Inputs]),
+							self() ! {learn, {{LayerId, self()}, DesiredOutput}};					
 						{false, null} -> true
 					end								
 			end,
 			perceptron(LayerId,Weights, New_Inputs, Sensitivities);
 		{connect_input, SenderNeuron_PID} ->
 			NewInputs = [{SenderNeuron_PID, 0.5} | Inputs],
-			io:format("~w output connected to ~w: ~w~n", [{LayerId,self()}, SenderNeuron_PID, NewInputs]),
+			%io:format("~w output connected to ~w: ~w~n", [{LayerId,self()}, SenderNeuron_PID, NewInputs]),
+			logger:debug_ann(loggerId, io_lib:format("[DEBUG][~w] ~w output connected to ~w: ~w", [?MODULE, {LayerId,self()}, SenderNeuron_PID, NewInputs])),
 			perceptron(LayerId, [0.5 | Weights], NewInputs, Sensitivities);
 		{connecto_output, ReceiverNeuron_PID} ->
 			NewOutput_PIDs = [{ReceiverNeuron_PID, 0} | Sensitivities],
-			io:format("~w output connected to ~w: ~w~n", [{LayerId,self()}, ReceiverNeuron_PID, NewOutput_PIDs]),
+			%io:format("~w output connected to ~w: ~w~n", [{LayerId,self()}, ReceiverNeuron_PID, NewOutput_PIDs]),
+			logger:debug_ann(loggerId, io_lib:format("[DEBUG][~w] ~w output connected to ~w: ~w", [?MODULE, {LayerId,self()}, ReceiverNeuron_PID, NewOutput_PIDs])),
 			perceptron(LayerId, Weights, Inputs, NewOutput_PIDs);
 		{propagate, Input, IsTraining} ->
-			io:format("Propagating input ~w ~n", [Input]),
+			%io:format("Propagating input ~w ~n", [Input]),
+			logger:debug_ann(loggerId, io_lib:format("[DEBUG][~w] Propagating input ~w ", [?MODULE, Input])),
 			stimulate_next_layer(LayerId, convert_to_keys(Sensitivities), Input, IsTraining),
 			perceptron(LayerId, Weights, Inputs, Sensitivities);
 		{learn, Backprop} ->
 			LearnRate = 0.5,
-						
+			
 			%%get the new sensitivites
 			NewSensitivites = add_sensitivity(Sensitivities, Backprop),
 			%%calculate output of the perceptron
@@ -182,8 +185,8 @@ perceptron(LayerId, Weights, Inputs, Sensitivities) ->
 			DxValue = feed_forward(Sigmoid_dx, Weights, convert_to_values(Inputs)),
 			Sensitivity = calculate_sensitivity(Backprop, Inputs, NewSensitivites, Output, DxValue),
 			
-			io:format("(~w) New Sensitivities: ~w~n", [{LayerId,self()}, NewSensitivites]),
-            io:format("(~w) Calculated Sensitivity: ~w~n", [{LayerId,self()}, Sensitivity]),
+			%io:format("(~w) New Sensitivities: ~w~n", [{LayerId,self()}, NewSensitivites]),
+            %io:format("(~w) Calculated Sensitivity: ~w~n", [{LayerId,self()}, Sensitivity]),
 			
 			%% adjust weights
 			
@@ -191,7 +194,7 @@ perceptron(LayerId, Weights, Inputs, Sensitivities) ->
 									   end,
 									   convert_to_values(Inputs)),
 			NewWeights = vector_map(fun(W, D) -> W + D end, Weights, WeightDif),
-			io:format("(~w) Adjusted Weights: ~w~n", [{LayerId,self()}, NewWeights]),
+			%io:format("(~w) Adjusted Weights: ~w~n", [{LayerId,self()}, NewWeights]),
 			
 			%%propagate value and weights to previous layer
 			vector_map(fun(Weight, {_InputNeuronId, InputNeuron_PID}) ->
@@ -202,6 +205,7 @@ perceptron(LayerId, Weights, Inputs, Sensitivities) ->
 			perceptron(LayerId, NewWeights, Inputs, NewSensitivites);
 		{status} ->
 			io:format("Status of Node ~w ~n W: ~w~n I: ~w~n S: ~w~n",[{LayerId,self()}, Weights, Inputs, Sensitivities]),
+			logger:debug_ann(loggerId, io_lib:format("[STATUS][~w] Status of Node ~w ~n W: ~w~n I: ~w~n S: ~w",[?MODULE, {LayerId,self()}, Weights, Inputs, Sensitivities])),
 			perceptron(LayerId, Weights, Inputs, Sensitivities);
 		{get_connections, CallerPid} ->
 			reply(CallerPid, {Weights, Inputs, Sensitivities}),
@@ -212,7 +216,8 @@ perceptron(LayerId, Weights, Inputs, Sensitivities) ->
 		{update_connections, Data} ->
 			%io:format("Update incoming~n"),
 			{NewWeights, NewInputs, NewSensitivities} = Data,
-			io:format("PERCEPTRON ~w new connections ~w~n", [LayerId, {NewWeights, NewInputs, NewSensitivities}]),
+			%io:format("PERCEPTRON ~w new connections ~w~n", [LayerId, {NewWeights, NewInputs, NewSensitivities}]),
+			logger:debug_ann(loggerId, io_lib:format("[DEBUG][~w] PERCEPTRON ~w new connections ~w~n", [?MODULE, LayerId, {NewWeights, NewInputs, NewSensitivities}])),
 			perceptron(LayerId, NewWeights, NewInputs, NewSensitivities);
 		{checkpoint, CallerPid, LogData} ->
 			save_training(LayerId, Weights, Inputs, Sensitivities, LogData),
@@ -254,10 +259,11 @@ convert_to_keys(Inputs) ->
 			  Inputs).
 
 stimulate_next_layer(_LayerId, [], _Output, _IsTraining) ->
-	io:format("Stimulating ENDED~n"),
+	%io:format("Stimulating ENDED~n"),
 	[];
 stimulate_next_layer(LayerId, [{_N_id, N_Pid} | N_Tail], Input, IsTraining) ->
 	io:format("Stimulating ~w with ~w~n", [N_Pid, Input]),
+	logger:debug_ann(loggerId, io_lib:format("[DEBUG][~w] Stimulating ~w with ~w", [?MODULE, N_Pid, Input])),
 	N_Pid ! {stimulate, {{LayerId, self()}, Input}, IsTraining},
 	stimulate_next_layer(LayerId, N_Tail, Input, IsTraining).
 	
@@ -273,6 +279,7 @@ calculate_sensitivity(_Backprop, Inputs, Sensitivities, _Output, _DxValue)
 calculate_sensitivity(Backprop, Inputs, Sensitivities, Output, DxValue)
 	when Sensitivities =:= [], Inputs =/= [] -> %%when output node
 		{_, TrainingValue} = Backprop,
+		%io:format("Calculating sensitivites for output node. Operation (~w - ~w) * ~w~n",[TrainingValue, Output, DxValue]),		
 		(TrainingValue - Output) * DxValue;
 calculate_sensitivity(_Backprop, Inputs, Sensitivities, _Output, DxValue)
 	when Sensitivities =/= [], Inputs =/= [] -> %%when hiden node
@@ -315,7 +322,12 @@ get_desired_output(NeuronId, DesiredOutputList) ->
 		lists:nth(OutputIndex, DesiredOutputList)
 	end.
 	
-	
+logger(NeuronId, Output, DesiredOutput) ->
+	{ok, Cwd} = file:get_cwd(),
+    Log = Cwd ++ "/logs/nn_train_log.txt",    
+    filemanager:write_result(Log, io_lib:format("NeuronId ~w output: ~w ... Desired output: ~w",[NeuronId,Output, DesiredOutput])),
+    true.
+    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%client interface functions%%%%%%%%%%%
 
@@ -330,6 +342,7 @@ save(NeuronPid, LogPath, IsCheckpoint) ->
 			{ok, saved};
 		Other ->
 			io:format("Error on neuron WAAAAAAAAAA ~w~n", [Other]),
+			logger:debug_ann(loggerId, io_lib:format("[DEBUG][~w] Error on neuron WAAAAAAAAAA ~w", [?MODULE,Other])),
 			{error, "bad save"}
 	end.
 
